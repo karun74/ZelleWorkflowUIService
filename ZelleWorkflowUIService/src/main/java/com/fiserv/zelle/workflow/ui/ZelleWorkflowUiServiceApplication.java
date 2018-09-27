14eb7a9d1fb12b570fd8bd0e0752562a7b0150b7
Your Name <you@example.com>
2018-09-27 17:22:08 -0500

    First commit

---
+++ ZelleWorkflowUIService/src/main/java/com/fiserv/zelle/workflow/ui/ZelleWorkflowUiServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fiserv.zelle.workflow.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZelleWorkflowUiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZelleWorkflowUiServiceApplication.class, args);
+	}
+}
